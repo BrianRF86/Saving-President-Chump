36faf370b4ed3748aa8726c0ff72410fa4814a5a
BrianRF86 <brianruiz.flynn@live.com>
2023-11-19 23:06:04 +0000

    Enemy seeks player AI

--- enemy.h
+++ enemy.h
@@ -33,4 +33,18 @@ y += speed_y;
             speed_x *= -1;
     }
 }
+
+// Enemy ai to chase player (testing with normal Vector2) https://stackoverflow.com/questions/28140867/getting-my-enemy-to-move-toward-my-player-c
+
+void EnemyPosition(Vector2 playerPosition){
+        //calculates direction from enemy to player
+        Vector2 direction = Vector2Subtract(playerPosition, (Vector2){x,y});
+
+        //Normalize the direction of the enemy
+        direction = Vector2Normalize(direction);
+
+        x += direction.x * speed_x;
+        y += direction.y * speed_y;
+
+}
 };
