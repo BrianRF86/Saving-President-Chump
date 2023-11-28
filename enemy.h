8a5b30ea090436ab896422ecb69c92d52ec18771
BrianRF86 <brianruiz.flynn@live.com>
2023-11-28 15:56:28 +0000

    created chase radius and speed instead of hitbox

--- enemy.h
+++ enemy.h
@@ -1,7 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 
-
+//updating enemy to include chase range https://jeannoelseneque.medium.com/game-development-with-raylib-c-day-5-87c6876eaba5
 
 //Ball https://github.com/BrianRF86/Git-hub-project/commit/ae34663f72f7e8ede5a7a12281c4a4ca0339929c
 
@@ -30,11 +30,19 @@ y += speed_y;
  //calculates direction from enemy to player
  Vector2 direction = Vector2Subtract(playerPosition, (Vector2){x,y});
 
+ //calculates distance between enemy to player
+ float distance = Vector2Distance (playerPosition,{ x, y});
+
+//defining chase multiplier
+ float chaseMultiplier = 1.5f;
+
+
+
  //Normalize the direction of the enemy
  direction = Vector2Normalize(direction);
 
- x += direction.x * speed_x * 1.75f;
- y += direction.y * speed_y * 1.75f;
+ x += direction.x * speed_x * chaseMultiplier;
+ y += direction.y * speed_y * chaseMultiplier;
 
 // Border collision
 
