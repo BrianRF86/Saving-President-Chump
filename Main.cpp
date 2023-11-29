5df929ff48db7eae696972b69c6a26eecb0e095e
BrianRF86 <brianruiz.flynn@live.com>
2023-11-29 18:05:48 +0000

    update code

--- Main.cpp
+++ Main.cpp
@@ -40,6 +40,7 @@ int main() {
 InitAudioDevice ();
 //Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6462">Pixabay</a>
 Sound Jump = loadSound("resources/jump.mp3");
+//Sound Effect from <a href="https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6082">Pixabay</a>
 Sound GameM = LoadSound("resources/gamem.mp3");
 
     // Initialize the Window
